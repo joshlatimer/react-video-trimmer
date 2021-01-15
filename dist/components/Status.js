49022704033cfa75e00ca5f055ac74d83a381121
Josh Latimer <joshgesh@gmail.com>
2021-01-15 02:13:43 +0000

    duh

---
+++ dist/components/Status.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+var Status = function Status(_ref) {
+  var children = _ref.children;
+  return React.createElement("div", {
+    className: "rvt-status"
+  }, children);
+};
+
+export default Status;
